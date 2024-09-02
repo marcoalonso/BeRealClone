98e22e48305b3a5b40950d9a5b291f92bda2579d
Marco Alonso <marcoalonsorota@hotmail.com>
2024-09-02 08:49:43 -0600

    Initial Commit

---
+++ BeRealClone/BeRealCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeRealCloneApp.swift
+//  BeRealClone
+//
+//  Created by Marco Alonso on 02/09/24.
+//
+
+import SwiftUI
+
+@main
+struct BeRealCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
