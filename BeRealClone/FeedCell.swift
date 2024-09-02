9463a87376ad497a964a10ed194ab2cf25d93e07
Marco Alonso <marcoalonsorota@hotmail.com>
2024-09-02 09:19:18 -0600

    FeedCell

---
+++ BeRealClone/FeedCell.swift
@@ -0,0 +1,18 @@
+//
+//  FeedCell.swift
+//  BeRealClone
+//
+//  Created by Marco Alonso on 02/09/24.
+//
+
+import SwiftUI
+
+struct FeedCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedCell()
+}
