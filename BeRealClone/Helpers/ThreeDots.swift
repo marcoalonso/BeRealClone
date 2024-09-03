bdc8e55eea72be867a420eed3bca51cf82e56fe3
Marco Alonso <marcoalonsorota@hotmail.com>
2024-09-03 11:03:17 -0600

    Module 03 FeedView

---
+++ BeRealClone/Helpers/ThreeDots.swift
@@ -0,0 +1,18 @@
+//
+//  ThreeDots.swift
+//  BeRealClone
+//
+//  Created by Marco Alonso on 02/09/24.
+//
+
+import SwiftUI
+
+struct ThreeDots: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThreeDots()
+}
